5939fb0b01ce6322489654217057c75b1574e140
Matthew Pin <MattTPin@gmail.com>
2024-07-12 16:39:48 -0700

    added defaults to required fields. Added uuid system to curator converter

--- conversion/curator_conversion/testing/batch_test.py
+++ conversion/curator_conversion/testing/batch_test.py
@@ -6,8 +6,9 @@ import glob
 
 # Define the input and output directories
 input_dir = "test_input_jsons"
+schema_file = "../../../json_schema/npmrd-exchange_schema.json"
+assignment_uuid_spreadsheet = "../assignment_uuid_master_list.csv"
 output_dir = "test_output"
-schema_file = "../../json_schema/npmrd-exchange_schema.json"
 
 # Find all .json files in the input directory
 json_files = glob.glob(os.path.join(input_dir, "*.json"))
@@ -24,7 +25,7 @@ for input_file in json_files:
     output_file = os.path.join(output_dir, new_file_name)
     
     # Create the command
-    command = f"python ../npmrd_curator_converter.py {input_file} {schema_file} {output_file}"
+    command = f"python ../npmrd_curator_converter.py {input_file} {schema_file} {assignment_uuid_spreadsheet} {output_file}"
     
     # Print the command (or you could use os.system(command) to run it directly)
     print(command)
--- standardization/testing/run_standardizer.py
+++ standardization/testing/run_standardizer.py
@@ -11,6 +11,10 @@ json_file_path = os.path.join("./test_jsons/article_json_1.json")
 with open(json_file_path, 'r') as file:
     json_dict = json.load(file)
 
+print("json_dict is")
+print(json_dict)
+print(type(json_dict))
+
 standardizer = JSONStandardizer(json_dict)
 standardition_results = standardizer.standardize()
 
