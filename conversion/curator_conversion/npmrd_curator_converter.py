7271d6e832a74ca0ab2b964c11089c74479f072f
Matthew Pin <MattTPin@gmail.com>
2024-08-09 12:08:49 -0700

    added embargo to assignment_data, removed assignment_master_uuid list

--- conversion/curator_conversion/npmrd_curator_converter.py
+++ conversion/curator_conversion/npmrd_curator_converter.py
@@ -5,6 +5,14 @@ import pandas as pd
 import uuid
 import jsonschema
 
+# import exchange_schema_file 
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define the path to the schema file one directory level up from the script's directory
+schema_file_path = os.path.join(script_dir, '..', '..', 'json_schema', 'npmrd-exchange_schema.json')
+
 # How to run from command line
 # python process_json.py input.json json_schema/npmrd-exchange_schema.json output.json
 # Arguments:
@@ -15,54 +23,23 @@ import jsonschema
 
 
 class CuratorConverter:
-    def __init__(self, schema_file, input_file, assignment_uuid_spreadsheet_file, output_file):
-        self.schema_file = schema_file
-        self.input_file = input_file
-        self.assignment_uuid_spreadsheet_file = assignment_uuid_spreadsheet_file
-        self.output_file = output_file
-        self.schema = self.load_json(schema_file)
-        self.input_json = self.load_json(input_file)
-        self.assignment_uuid_spreadsheet = self.load_spreadsheet(assignment_uuid_spreadsheet_file)
+    def __init__(self, curator_json_dict):
+        self.curator_json_dict = curator_json_dict
+        self.schema = self.load_schema_json(schema_file_path)
 
     @staticmethod
-    def load_json(file_path):
+    def load_schema_json(file_path):
         """Load JSON data from a file."""
         try:
             with open(file_path, 'r') as file:
-                data = json.load(file)
-            return data
+                schema = json.load(file)
+            return schema
         except FileNotFoundError:
-            print(f"The file at {file_path} was not found.")
-        except json.JSONDecodeError:
-            print("Error decoding the JSON file.")
+            print(f"The schema file was not found at `{file_path}`")
+        except json.JSONDecodeError as e:
+            print(f"Error decoding the JSON file: {e}")
         except Exception as e:
             print(f"An error occurred: {e}")
-
-    @staticmethod
-    def save_json(data, file_path):
-        """Save JSON data to a file."""
-        with open(file_path, 'w') as file:
-            json.dump(data, file, indent=4)
-
-    @staticmethod
-    def load_spreadsheet(file_path):
-        """Load spreadsheet data from a CSV file into a Pandas DataFrame."""
-        try:
-            df = pd.read_csv(file_path)
-            return df
-        except FileNotFoundError:
-            print(f"The file at {file_path} was not found.")
-        except pd.errors.EmptyDataError:
-            print(f"The file at {file_path} is empty.")
-        except pd.errors.ParserError:
-            print(f"Error parsing the file '{file_path}'")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-    @staticmethod
-    def save_spreadsheet(data, file_path):
-        """Save DataFrame data to a CSV file."""
-        data.to_csv(file_path, index=False)
 
     def generate_empty_value(self, schema):
         schema_type = schema.get("type")
@@ -102,12 +79,12 @@ class CuratorConverter:
     def get_empty_spectrum_from_schema(self):
         return self.generate_empty_value(self.schema)['nmr_data']['assignment_data'][0]['spectrum'][0]
 
-    def build_final_json(self):
+    def convert_json(self):
         """Update the schema with data from the input JSON."""
         final_json_list = []
 
         # Iterate through full input json
-        for curator_entry in self.input_json:
+        for curator_entry in self.curator_json_dict:
             new_json = self.generate_json_from_schema()
 
             new_json['compound_name'] = curator_entry['name']
@@ -116,6 +93,9 @@ class CuratorConverter:
             new_json['citation']['doi'] = curator_entry['origin_doi']
             new_json['origin']['genus'] = curator_entry['origin_genus']
             new_json['origin']['species'] = curator_entry['origin_species']
+            new_json['submission']['source'] = "npmrd_curator"
+            new_json['nmr_data']['experimental_data']['nmr_metadata'] = []
+            new_json['nmr_data']['peak_lists'] = []
             new_json['submission']['source'] = "npmrd_curator"
 
             has_assignment_data = False
@@ -128,11 +108,7 @@ class CuratorConverter:
             if len(curator_entry['c_nmr']['spectrum']) > 0:
                 has_assignment_data = True
                 new_assignment_entry = self.get_empty_assignment_data_from_schema()
-                new_assignment_entry['assignment_uuid'] = self.determine_assignment_uuid(
-                    curator_entry['smiles'],
-                    curator_entry['origin_doi'],
-                    "C"
-                )
+                new_assignment_entry['assignment_uuid'] = None
                 new_assignment_entry['nucleus'] = "C"
                 new_assignment_entry['solvent'] = curator_entry['c_nmr']['solvent']
                 new_assignment_entry['temperature'] = curator_entry['c_nmr']['temperature']
@@ -144,7 +120,6 @@ class CuratorConverter:
                 new_spectrum_list = []
                 for curator_c_spectrum in curator_entry['c_nmr']['spectrum']:
                     new_spectrum_entry = self.get_empty_spectrum_from_schema()
-                    print("H empty get_empty_assignment_data_from_schema is", new_spectrum_entry)
                     new_spectrum_entry['shift'] = curator_c_spectrum['shift']
                     new_spectrum_entry['atom_index'] = curator_c_spectrum['atom_index']
                     new_spectrum_entry['rdkit_index'] = [curator_c_spectrum['rdkit_index']]
@@ -160,11 +135,7 @@ class CuratorConverter:
                 has_assignment_data = True
                 new_assignment_entry = self.get_empty_assignment_data_from_schema()
                 
-                new_assignment_entry['assignment_uuid'] = self.determine_assignment_uuid(
-                    curator_entry['smiles'],
-                    curator_entry['origin_doi'],
-                    "H"
-                )
+                new_assignment_entry['assignment_uuid'] = None
                 new_assignment_entry['nucleus'] = "H"
                 new_assignment_entry['solvent'] = curator_entry['h_nmr']['solvent']
                 new_assignment_entry['temperature'] = curator_entry['h_nmr']['temperature']
@@ -176,7 +147,6 @@ class CuratorConverter:
                 new_spectrum_list = []
                 for curator_h_spectrum in curator_entry['h_nmr']['spectrum']:
                     new_spectrum_entry = self.get_empty_spectrum_from_schema()
-                    print("H empty get_empty_assignment_data_from_schema is", new_spectrum_entry)
                     new_spectrum_entry['shift'] = curator_h_spectrum['shift']
                     new_spectrum_entry['multiplicity'] = curator_h_spectrum['multiplicity']
                     new_spectrum_entry['coupling'] = curator_h_spectrum['coupling']
@@ -204,67 +174,17 @@ class CuratorConverter:
                 print(f"WARNING: NO ASSIGNMENT DATA IN ENTRY {curator_entry['name']} / {curator_entry['origin_doi']}")
 
         return final_json_list
-
-    def determine_assignment_uuid(self, smiles, doi, nucleus):
-        """Determine or generate assignment UUIDs for a list."""
-        result_uuids = []
-
-        match = self.assignment_uuid_spreadsheet[
-            (self.assignment_uuid_spreadsheet['smiles'] == smiles) &
-            (self.assignment_uuid_spreadsheet['doi'] == doi) &
-            (self.assignment_uuid_spreadsheet['nucleus'] == nucleus)
-        ]
-
-        if match.empty:
-            assignment_uuid = str(uuid.uuid4())
-
-            new_spreadsheet_entry = pd.DataFrame({
-                'smiles': [smiles],
-                'doi': [doi],
-                'nucleus': [nucleus],
-                'assignment_uuid': assignment_uuid
-            })
-
-            self.assignment_uuid_spreadsheet = pd.concat([
-                self.assignment_uuid_spreadsheet,
-                new_spreadsheet_entry
-            ], ignore_index=True)
-            
-        else:
-            assignment_uuid = match.iloc[0]['assignment_uuid']
-
-        return assignment_uuid
     
     def validate_exchange_json(self, json):
         try:
             jsonschema.validate(instance=json, schema=self.schema)
-            print("Validation successful. Data conforms to schema.")
         except jsonschema.exceptions.ValidationError as e:
             # Print the validation error message
             print(f"Validation error: {e.message}")
             # Raise an exception to stop further execution
             raise e
-        
-    def convert_json(self):
-        updated_data = self.build_final_json()
-        self.save_spreadsheet(self.assignment_uuid_spreadsheet, self.assignment_uuid_spreadsheet_file)
-        self.save_json(updated_data, self.output_file)
-        print(f"-- Generated Exchange JSON: '{self.output_file}'")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process a JSON file and update schema.')
-    parser.add_argument('input_file', type=str, help='The path to the input JSON file.')
-    parser.add_argument('schema_file', type=str, help='The path to the JSON schema file.')
-    parser.add_argument('assignment_uuid_spreadsheet_file', type=str, help='The path to the assignment uuid master list spreadsheet.')
-    parser.add_argument('output_file', type=str, help='The path to save the updated JSON file.')
-
-    args = parser.parse_args()
-
-    curator_converter = CuratorConverter(
-        args.schema_file,
-        args.input_file,
-        args.assignment_uuid_spreadsheet_file,
-        args.output_file
-    )
-    curator_converter.convert_json()
+# Example usage
+# curator_converter = CuratorConverter(curator_json_dict)
+# npmrd_exchange_dict = curator_converter.convert_json()
--- conversion/curator_conversion/testing/batch_test.py
+++ conversion/curator_conversion/testing/batch_test.py
@@ -1,32 +1,40 @@
 import os
 import glob
+import json
+from ..npmrd_curator_converter import CuratorConverter
+# from conversion.curator_conversion.npmrd_curator_converter import CuratorConverter
+# from conversion.curator_conversion.npmrd_curator_converter import CuratorConverter
+
 
 # First deletes all files in /test_output then runs npmrd_curator_converter.py
 # on all .json files in test_input_jsons with the outputs being places in /test_output
 
 # Define the input and output directories
-input_dir = "test_input_jsons"
-schema_file = "../../../json_schema/npmrd-exchange_schema.json"
-assignment_uuid_spreadsheet = "../assignment_uuid_master_list.csv"
-output_dir = "test_output"
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define the input and output directories relative to the script directory
+input_dir = os.path.join(script_dir, "test_input_jsons")
+output_dir = os.path.join(script_dir, "test_output")
 
 # Find all .json files in the input directory
 json_files = glob.glob(os.path.join(input_dir, "*.json"))
 
 # Generate and run the batch commands
 for input_file in json_files:
-    # Split the input_file into folder path and file name
+    # Load input file as a dict
+    with open(input_file, 'r') as file:
+        curator_json_dict = json.load(file)
+    
+    curator_converter = CuratorConverter(curator_json_dict)
+    npmrd_exchange_dict = curator_converter.convert_json()
+    
+    # Generate the output path
     folder_path, file_name = os.path.split(input_file)
-    
-    # Append "exchange_" to the file name
     new_file_name = "exchange_" + file_name
-    
-    # Combine the output directory with the new file name
-    output_file = os.path.join(output_dir, new_file_name)
-    
-    # Create the command
-    command = f"python ../npmrd_curator_converter.py {input_file} {schema_file} {assignment_uuid_spreadsheet} {output_file}"
-    
-    # Print the command (or you could use os.system(command) to run it directly)
-    print(command)
-    os.system(command)
+    output_file_path = os.path.join(output_dir, new_file_name)
+
+    # Write the dictionary to the new JSON file
+    with open(output_file_path, 'w') as outfile:
+        json.dump(npmrd_exchange_dict, outfile, indent=4)
+    print(f"successfully converted {output_file_path}")
